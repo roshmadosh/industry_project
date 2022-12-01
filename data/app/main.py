b3d944884d1d26cf6d899a7fb58f5ba888cd1488
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-12-01 11:04:46 -0500

    data endpoint for running models

--- data/app/main.py
+++ data/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    id: int
+    name: str
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/data/")
+async def predict(item: Item):
+    # add data scienc-y stuff here...
+    return item
