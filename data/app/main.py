2fdb715283e34a476bff7d8e9503255ef3347764
roshmadosh <hiroshi.nobuoka@gmail.com>
2022-12-02 11:14:52 -0500

    updated docker scripts

--- data/app/main.py
+++ data/app/main.py
@@ -3,11 +3,24 @@ from pydantic import BaseModel
 from .graph import bell_curve_salary_to_rent_plot
 from PIL import Image
 import io
+from fastapi.middleware.cors import CORSMiddleware
   
 # open method used to open different extension image file
 
+origins = [
+    '*'
+]
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
